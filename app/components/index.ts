954bc4ab9373d9571ef8ee707c6e303aa57a837b
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-05-01 22:08:32 +0530

    Added - basic folder structure, components and support packages

---
+++ app/components/index.ts
@@ -0,0 +1,15 @@
+export { default as UITextView } from "./UITextView";
+export { default as UIContainer } from "./UIContainer";
+export { default as UILoader } from "./UILoader";
+export { default as UIAlert } from "./UIAlert";
+
+// buttons
+export { default as UIButton } from "./Buttons/UIButton";
+export { default as UIFAB } from "./Buttons/UIFloatingActionButton";
+export { default as UIIconButton } from "./Buttons/UIIconButton";
+
+// text input fields
+export { default as UITextInput } from "./UITextInput";
+
+// image
+export { default as UIImage } from "./UIImage";
---
+++ app/constants/app_icons.ts
@@ -0,0 +1,15 @@
+import EntypoIcons from "@expo/vector-icons/Entypo";
+import FontistoIcon from "@expo/vector-icons/Fontisto";
+import MaterialCommunityIcon from "@expo/vector-icons/MaterialCommunityIcons";
+import IoniconIcon from "@expo/vector-icons/Ionicons";
+import AntDesignIcon from "@expo/vector-icons/AntDesign";
+import FontAwesomeIcon from "@expo/vector-icons/FontAwesome5";
+
+export default {
+  EntypoIcons,
+  FontistoIcon,
+  MaterialCommunityIcon,
+  IoniconIcon,
+  AntDesignIcon,
+  FontAwesomeIcon,
+};
